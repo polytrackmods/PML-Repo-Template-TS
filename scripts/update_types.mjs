469a9fa2b66d8bf91d1f8367eb07dd13a9d4b663
CRJakob <wallner.jakob@icloud.com>
2026-08-09 15:20:43 +0200

    Switched from submodule to script to fetch polytypes

---
+++ scripts/update_types.mjs
@@ -0,0 +1,16 @@
+import fs from 'node:fs/promises';
+
+const polytypes_url = "https://git.polymodloader.com/polytrackmods/PolyModLoader/raw/branch/0.6.2/PolyTypes.d.ts"
+
+async function update_polytypes() {
+    const polytypes_raw = await fetch(polytypes_url)
+    const polytypes = await polytypes_raw.text()
+    if (!polytypes_raw.ok) {
+        console.error('Failed to fetch PolyTypes type definition')
+        return
+    }
+    await fs.writeFile('PolyTypes.d.ts', polytypes)
+    console.log('Wrote PolyTypes type definition to PolyTypes.d.ts')
+}
+
+await update_polytypes()
